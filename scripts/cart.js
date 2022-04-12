c9e8f1cbf790067a3913c0a7a61f090b2a67384f
Fran Marić <fran.maric.vk@gmail.com>
2022-04-12 11:07:34 +0200

    Fix items undefined

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,7 +1,7 @@
 function getCart() {
-    //INSERT CODE HERE - Zadatak
-  
-   //END INSERT CODE - Zadatak
+	let	cartItems = JSON.parse(localStorage.getItem(cartItemsKey));
+
+    console.log(cartItems);
 }
 
  let refreshCart = async function () {
@@ -11,30 +11,30 @@ function getCart() {
         if(ids.length < 1) return;
         let container = document.querySelector('.cart');
         container.innerHTML = "";
-        
+
         let cartHeaderTemplate = document.querySelector('#cart-template-header');
         let cartHeader = cartHeaderTemplate.content.cloneNode(true);
         container.appendChild(cartHeader);
-        
+
         //INSERT CODE HERE - Zadatak
-        
+
         //END INSERT CODE - Zadatak
-        
+
         let data = await response.json();
         let cartItemTemplate = document.querySelector('#cart-template-item');
         for(const id of ids){
             let product = data.products.find(p => p.id == id);
-            
+
             let cartItem = cartItemTemplate.content.cloneNode(true);
-            
+
             cartItem.querySelector(".cart-item").dataset.id = id;
             let title = cartItem.querySelector('.cart-item-title');
             title.textContent = product.name;
             let quantity = cartItem.querySelector('.cart-item-quantity');
             quantity.value = cart[id];
-                
+
             //INSERT CODE HERE - Zadatak
-            
+
             //END INSERT CODE - Zadatak
 
             container.appendChild(cartItem);
