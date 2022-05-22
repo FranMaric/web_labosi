4a26ecac2c5cce3c4abcb540716e6418cbe30c7f
Fran Marić <fran.maric.vk@gmail.com>
2022-05-22 18:06:57 +0200

    Add wrong id handling

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -6,6 +6,11 @@ router.get('/:id([0-9]{1,20})', async function(req, res, next) {
     let id = parseInt(req.params.id);
     let item = await InventoryRepository.getItem(id);
 
+    if (item === null) {
+        res.status(404).send(`There is no item with id ${id} :(`);
+        return;
+    }
+
     res.render('item', {
         title: item.name,
         linkActive: 'item',
