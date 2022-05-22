a0049bda76969717b1d30c323f39f0cf078c5b5c
Fran Marić <fran.maric.vk@gmail.com>
2022-05-22 18:23:50 +0200

    Functional

--- db/inventory_repository.js
+++ db/inventory_repository.js
@@ -2,31 +2,17 @@ const { query } = require("./db");
 
 class InventoryRepository {
     static async getItem(itemId) {
-        let items = await query(`SELECT * FROM inventory WHERE id=${itemId}`);
+        let items = await query(`select inventory.*, categories.name as categoryName, categories.description as categoryDescription, categories.seasonal
+                                    from inventory
+                                    left join categories
+                                    on inventory.categoryid = categories.id
+                                    where inventory.id = ${itemId}`);
 
         if (items.rows.length === 0) {
             return null;
         }
 
-        let item = items.rows[0];
-
-        let category = await InventoryRepository.getCategory(item.categoryid);
-
-        item.categoryName = category.name;
-        item.seasonal = category.seasonal;
-
-        return item;
-    }
-
-    static async getCategory(categoryId) {
-        let categories = await query(`SELECT * FROM categories WHERE id=${categoryId}`);
-
-        if (categories.rows.length === 0) {
-            return null;
-        }
-
-        return categories.rows[0];
-
+        return items.rows[0];
     }
 
     static async getItems(categoryId) {
