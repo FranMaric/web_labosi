1e3b5758d4dde9e87c6502a6d52749e4f266575d
Fran Marić <fran.maric.vk@gmail.com>
2022-05-22 17:59:59 +0200

    in progress

--- db/inventory_repository.js
+++ db/inventory_repository.js
@@ -1,8 +1,32 @@
 const { query } = require("./db");
 
 class InventoryRepository {
-    getItem(itemId) {
-        // query("SELECT * FROM");
+    static async getItem(itemId) {
+        let items = await query(`SELECT * FROM inventory WHERE id=${itemId}`);
+
+        if (items.rows.length === 0) {
+            return null;
+        }
+
+        let item = items.rows[0];
+
+        let category = await InventoryRepository.getCategory(item.categoryid);
+
+        item.categoryName = category.name;
+        item.seasonal = category.seasonal;
+
+        return item;
+    }
+
+    static async getCategory(categoryId) {
+        let categories = await query(`SELECT * FROM categories WHERE id=${categoryId}`);
+
+        if (categories.rows.length === 0) {
+            return null;
+        }
+
+        return categories.rows[0];
+
     }
 
     static async getItems(categoryId) {
--- routes/item.routes.js
+++ routes/item.routes.js
@@ -1,11 +1,16 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var { InventoryRepository } = require('../db/inventory_repository');
+var router = express.Router();
 
-// router.get('/', function(req, res, next) {
-//     res.render('item', {
-//         title: 'Home',
-//         linkActive: 'item'
-//     });
-// });
+router.get('/:id([0-9]{1,20})', async function(req, res, next) {
+    let id = parseInt(req.params.id);
+    let item = await InventoryRepository.getItem(id);
 
-// module.exports = router;
+    res.render('item', {
+        title: item.name,
+        linkActive: 'item',
+        item: item,
+    });
+});
+
+module.exports = router;
